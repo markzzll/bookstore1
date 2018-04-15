d0f41e02a003685fef4702cdabc54a367421d18e
markzhanglei <1158098117@qq.com>
2018-04-15 16:53:29 +0800

    书城

---
+++ bookstore/books/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import tinymce.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Books',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('is_delete', models.BooleanField(verbose_name='删除标记', default=False)),
+                ('create_time', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('update_time', models.DateTimeField(verbose_name='更新时间', auto_now=True)),
+                ('type_id', models.SmallIntegerField(verbose_name='商品种类', choices=[(1, 'Python'), (2, 'Javascript'), (3, '数据结构与算法'), (4, '机器学习'), (5, '操作系统'), (6, '数据库')], default=1)),
+                ('name', models.CharField(verbose_name='商品名称', max_length=20)),
+                ('desc', models.CharField(verbose_name='商品简介', max_length=128)),
+                ('price', models.DecimalField(verbose_name='商品价格', decimal_places=2, max_digits=10)),
+                ('unit', models.CharField(verbose_name='商品单位', max_length=20)),
+                ('stock', models.IntegerField(verbose_name='商品库存', default=1)),
+                ('sales', models.IntegerField(verbose_name='商品销量', default=0)),
+                ('detail', tinymce.models.HTMLField(verbose_name='商品详情')),
+                ('image', models.ImageField(verbose_name='商品图片', upload_to='books')),
+                ('status', models.SmallIntegerField(verbose_name='商品状态', choices=[(0, '下线'), (1, '上线')], default=1)),
+            ],
+            options={
+                'db_table': 's_books',
+            },
+        ),
+    ]
